505950fe7c601864fda6f66008c86ba117a494be
Necip Allef <necipallef@gmail.com>
2024-01-09 11:22:26 +0300

    fix: try catch for missing ConfigStore and other general cases

--- src/index.ts
+++ src/index.ts
@@ -3,17 +3,23 @@ import {handleReq} from "./handler";
 import {IntegrationEnv} from "./env";
 import { ConfigStore } from "fastly:config-store";
 import {returnHttpResponse} from "./utils/returnHttpResponse";
+import {createFallbackErrorResponse} from "./utils";
 
 addEventListener("fetch", (event) => event.respondWith(handleRequest(event)));
 
 async function handleRequest(event: FetchEvent): Promise<Response> {
-    const request = event.request;
-    const clientIp = event.client.address
-    const config = new ConfigStore('Fingerprint');
-    const envObj : IntegrationEnv = {
-        AGENT_SCRIPT_DOWNLOAD_PATH: config.get('AGENT_SCRIPT_DOWNLOAD_PATH'),
-        GET_RESULT_PATH: config.get('GET_RESULT_PATH'),
-        PROXY_SECRET: config.get('PROXY_SECRET')
+    try {
+        const request = event.request;
+        const clientIp = event.client.address
+        const config = new ConfigStore('Fingerprinta');
+        const envObj: IntegrationEnv = {
+            AGENT_SCRIPT_DOWNLOAD_PATH: config.get('AGENT_SCRIPT_DOWNLOAD_PATH'),
+            GET_RESULT_PATH: config.get('GET_RESULT_PATH'),
+            PROXY_SECRET: config.get('PROXY_SECRET')
+        }
+        return handleReq(request, envObj, clientIp).then(returnHttpResponse)
+    } catch (e) {
+        console.error(e)
+        return createFallbackErrorResponse('something went wrong')
     }
-    return handleReq(request, envObj, clientIp).then(returnHttpResponse)
 }
