b3bd766f120073a6199b825dafa7b151c8d1c359
Necip Allef <necipallef@gmail.com>
2024-01-09 11:29:39 +0300

    fix: use default values if missing config store

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 /// <reference types="@fastly/js-compute" />
 import {handleReq} from "./handler";
 import {IntegrationEnv} from "./env";
-import { ConfigStore } from "fastly:config-store";
+import {ConfigStore} from "fastly:config-store";
 import {returnHttpResponse} from "./utils/returnHttpResponse";
 import {createFallbackErrorResponse} from "./utils";
 
@@ -11,15 +11,33 @@ async function handleRequest(event: FetchEvent): Promise<Response> {
     try {
         const request = event.request;
         const clientIp = event.client.address
-        const config = new ConfigStore('Fingerprint');
-        const envObj: IntegrationEnv = {
-            AGENT_SCRIPT_DOWNLOAD_PATH: config.get('AGENT_SCRIPT_DOWNLOAD_PATH'),
-            GET_RESULT_PATH: config.get('GET_RESULT_PATH'),
-            PROXY_SECRET: config.get('PROXY_SECRET')
-        }
+        const envObj = getEnvObject()
         return handleReq(request, envObj, clientIp).then(returnHttpResponse)
     } catch (e) {
         console.error(e)
         return createFallbackErrorResponse('something went wrong')
+    }
+}
+
+function getEnvObject(): IntegrationEnv {
+    let config
+    try {
+        config = new ConfigStore('Fingerprint');
+    } catch (e) {
+        console.error(e)
+    }
+
+    if (config == null) {
+        return {
+            AGENT_SCRIPT_DOWNLOAD_PATH: null,
+            GET_RESULT_PATH: null,
+            PROXY_SECRET: null
+        }
+    }
+
+    return {
+        AGENT_SCRIPT_DOWNLOAD_PATH: config.get('AGENT_SCRIPT_DOWNLOAD_PATH'),
+        GET_RESULT_PATH: config.get('GET_RESULT_PATH'),
+        PROXY_SECRET: config.get('PROXY_SECRET')
     }
 }
