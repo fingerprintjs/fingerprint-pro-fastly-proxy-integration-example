7209334054708489f720886040d86cb36b68948a
Orkun <orkuncakilkaya@gmail.com>
2024-10-04 14:19:04 +0300

    chore: make default plugin disabled first

--- plugins/fingerprintKvStorage.ts
+++ plugins/fingerprintKvStorage.ts
@@ -1,14 +1,16 @@
-import { KVStore } from 'fastly:kv-store'
-import { PluginType, registerPlugin } from '../../../src/utils/registerPlugin'
-import { EventResponse } from '@fingerprintjs/fingerprintjs-pro-server-api'
+// To enable this plugin, please uncomment the code block below:
 
+/*
+import { KVStore } from 'fastly:kv-store'
+import { PluginType, registerPlugin } from '../src/utils/registerPlugin'
+import { EventResponse } from '@fingerprintjs/fingerprintjs-pro-server-api'
 async function fingerprintProcessUnsealedResult(requestId: string, data: EventResponse | null) {
   const store = new KVStore('FingerprintResults')
   await store.put(requestId, JSON.stringify(data))
 }
-
 registerPlugin({
   name: 'Fingerprint Process Unsealed Result with Fastly KV Storage',
   function: fingerprintProcessUnsealedResult,
   type: PluginType.ProcessUnsealedResult,
 })
+*/
