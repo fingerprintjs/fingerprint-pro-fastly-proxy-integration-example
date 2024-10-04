402498f6c1eba4c318d0f5a21d9dab679a745bfb
Orkun <orkuncakilkaya@gmail.com>
2024-10-04 15:02:04 +0300

    refactor: move example plugin to plugins/examples

--- plugins/examples/fingerprintKvStorage.ts
+++ plugins/examples/fingerprintKvStorage.ts
@@ -1,6 +1,6 @@
-// To enable this plugin, please uncomment the code block below:
+// To enable this plugin, please move it to: <project-root>./plugins/fingerprintKvStorage.ts
+// Don't forget to replace import paths after moving
 
-/*
 import { KVStore } from 'fastly:kv-store'
 import { PluginType, registerPlugin } from '../src/utils/registerPlugin'
 import { EventResponse } from '@fingerprintjs/fingerprintjs-pro-server-api'
@@ -8,9 +8,9 @@ async function fingerprintProcessUnsealedResult(requestId: string, data: EventRe
   const store = new KVStore('FingerprintResults')
   await store.put(requestId, JSON.stringify(data))
 }
+
 registerPlugin({
   name: 'Fingerprint Process Unsealed Result with Fastly KV Storage',
   function: fingerprintProcessUnsealedResult,
   type: PluginType.ProcessUnsealedResult,
 })
-*/
