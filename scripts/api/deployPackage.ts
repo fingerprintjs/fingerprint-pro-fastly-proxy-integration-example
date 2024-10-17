e3896143633ae23ad2733259568cc3a2df28820e
Orkun <orkuncakilkaya@gmail.com>
2024-10-17 12:20:17 +0300

    chore: ci fix deploy path for wasm file

--- scripts/api/deployPackage.ts
+++ scripts/api/deployPackage.ts
@@ -5,6 +5,6 @@ export async function deployPackage(service_id: string, versionId: number) {
   return createClient('package').putPackage({
     version_id: versionId,
     service_id,
-    _package: fs.createReadStream(__dirname + '/../../pkg/fingerprint-pro-fastly-proxy-integration.tar.gz'),
+    _package: fs.createReadStream(__dirname + '/../../bin/main.wasm'),
   })
 }
