3e9f1e9d266c5fb68e540b0d1d4932f88d5fe52e
Orkun <orkuncakilkaya@gmail.com>
2024-10-28 12:30:34 +0300

    fix: include plugins in build

--- build.ts
+++ build.ts
@@ -1,13 +1,36 @@
 import { build } from 'esbuild'
+import { readdirSync, writeFileSync, unlinkSync } from 'fs'
+import { join, extname, basename } from 'path'
 
-// Load environment variables from process.env
 const configStoreName = process.env.CONFIG_STORE_NAME || 'Fingerprint'
 
+function getPluginEntries(folder: string) {
+  const dirPath = join(__dirname, folder)
+  return readdirSync(dirPath)
+    .filter((file) => extname(file) === '.ts' && !basename(file).endsWith('.example.ts'))
+    .map((file) => `import '${join(dirPath, file).replace(/\\/g, '/')}';`)
+}
+
+const virtualEntry = './virtual-entry.ts'
+const pluginImports = getPluginEntries('./plugins').join('\n')
+const entryContent = `import './src/index.ts';\n${pluginImports}`
+
+writeFileSync(virtualEntry, entryContent)
+
 build({
-  entryPoints: ['./src/index.ts'],
-  outdir: './build',
+  entryPoints: [virtualEntry],
+  outfile: './build/index.js',
   bundle: true,
   format: 'cjs',
   external: ['fastly:*'],
   define: { 'process.env.CONFIG_STORE_NAME': `"${configStoreName}"` },
-}).catch(() => process.exit(1))
+  platform: 'node',
+  target: 'esnext',
+})
+  .then(() => {
+    unlinkSync(virtualEntry)
+  })
+  .catch((error) => {
+    console.error('Build failed:', error)
+    process.exit(1)
+  })
